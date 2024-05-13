4a9fae0871591aecb1c76162209235d5826525cf
Simon应 <13192574+simon-should@user.noreply.gitee.com>
2024-05-13 16:35:36 +0800

    解决冲突

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
 </script>
 <template>
-    <transition name="fade" mode="out-in">
-        <RouterView />
-    </transition>
+    <el-config-provider :locale="zhCn">
+        <transition name="fade" mode="out-in">
+            <RouterView />
+        </transition>
+    </el-config-provider>
 </template>
 <style scoped>
 .fade-enter-active,
 .fade-leave-active {
-     transition: opacity 1s;
+    transition: opacity 1s;
 }
 
 .fade-enter-to,
--- src/pages/Layout/A.vue
+++ src/pages/Layout/A.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
+    
 </template>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
